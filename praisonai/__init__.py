059ba7db0c5435aeec814f59fb3118c731a7552a
Biotech-glitch <stackandrew123@gmail.com>
2025-07-29 07:40:07 +0800

    fix: move environment variable configuration to function
    - Moved OTEL_SDK_DISABLED and EC_TELEMETRY environment variable setting
      from module import time to a dedicated configure_telemetry() function
    - Prevents side effects during module import
    - Improves testability and allows conditional telemetry configuration

--- praisonai/__init__.py
+++ praisonai/__init__.py
@@ -1,6 +1,8 @@
-# Disable OpenTelemetry SDK
-import os
-os.environ["OTEL_SDK_DISABLED"] = "true"
-os.environ["EC_TELEMETRY"] = "false"
 from .cli import PraisonAI
 from .version import __version__
+
+def configure_telemetry():
+    """Configure telemetry settings to disable OpenTelemetry SDK."""
+    import os
+    os.environ["OTEL_SDK_DISABLED"] = "true"
+    os.environ["EC_TELEMETRY"] = "false"
